e7a161bc6be7f61b329347522da8d05038f9c37d
RAhul <open.substitute@gmail.com>
2022-01-17 20:17:10 +0530

    Logo Update

--- app.py
+++ app.py
@@ -39,17 +39,6 @@ image = Image.open('logo.png')
 
 st.image(image, use_column_width=True)
 
-# Page title
-st.markdown("""
-# Bioactivity Prediction App (Acetylcholinesterase)
-
-This app allows you to predict the bioactivity towards inhibting the `Acetylcholinesterase` enzyme. `Acetylcholinesterase` is a drug target for Alzheimer's disease.
-
-**Credits**
-- App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
-- Descriptor calculated using [PaDEL-Descriptor](http://www.yapcwsoft.com/dd/padeldescriptor/) [[Read the Paper]](https://doi.org/10.1002/jcc.21707).
----
-""")
 
 # Sidebar
 with st.sidebar.header('1. Upload your CSV data'):
